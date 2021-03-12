f00d029ae58a8d1656372ee8664627bf51284833
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-12 14:54:48 +0200

    top navbar added + css style

---
+++ app/views/inc/topnav.php
@@ -0,0 +1,18 @@
+<header class="stick">
+    <nav class="p-3">
+        <div class="d-flex justify-content-between align-items-center">
+            <!-- MENU -->
+            <div>
+                <p class="m-0" id="slider"><i class="fas fa-bars m-2"></i></p>
+            </div>
+            <!--TITLE-->
+            <div class="title">
+                <a href="<?php echo URLROOT; ?>" class="nav-link">SOME SHOES</a>
+            </div>
+            <!-- CART -->
+            <div>
+                <a href="<?php echo URLROOT; ?>/carts/cart" class="nav-link"><i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+    </nav>
+</header>
