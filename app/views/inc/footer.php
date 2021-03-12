76227b7d4aee098855bd24f690238f8e89b24671
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-12 14:53:28 +0200

    added header and footer

---
+++ app/views/inc/footer.php
@@ -0,0 +1,16 @@
+<footer class="d-flex justify-content-center flex-column align-items-center">
+    <div class="d-flex justify-content-between align-items-center" id="social">
+        <a href="#" class="footerIcons" target="_blank"><i class="fab fa-facebook-f m-10"></i> FACEBOOK</a>
+        <a href="#" class="footerIcons" target="_blank"><i class="fab fa-twitter m-10"></i> TWITTER</a>
+        <a href="#" class="footerIcons" target="_blank"><i class="fab fa-instagram m-10"></i> INSTAGRAM</a>
+        <a href="#" class="footerIcons" target="_blank"><i class="fab fa-youtube m-10"></i> YOUTUBE</a>
+    </div>
+
+    <div class="fs-14" id="footerSecond">
+        <span>Gerda Baltrusaityte &copy; 2021</span>
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ app/views/inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo SITENAME; ?></title>
+    <script src="https://kit.fontawesome.com/42762d49f1.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <script src="<?php echo URLROOT; ?>/js/script.js" defer></script>
+</head>
+
+<body>
+
+    <?php require_once APPROOT . '/views/inc/topnav.php'; ?>
+    <?php require_once APPROOT . '/views/inc/sidenav.php'; ?>
