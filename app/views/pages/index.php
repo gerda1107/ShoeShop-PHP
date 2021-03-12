b921f7ae08dbd0fa183e277e6fe9a5d5d729eb58
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-03-12 15:01:44 +0200

    main page view finished

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,11 @@
-<?php
+<?php require APPROOT . '/views/inc/header.php'; ?>
 
-echo "This is Main Page";
+<section id="firstWindow">
+    <div class="bg-img d-flex justify-content-center align-items-end p-5">
+        <form method="POST">
+            <button id="shopNow" name="shopNowBtn">SHOP NOW</button>
+        </form>
+    </div>
+</section>
 
-?>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
